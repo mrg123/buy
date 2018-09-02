cde885b1e211600fa6e4dbebf82c337c8d720ca7
mrg123 <ccna8008@163.com>
2018-09-02 23:47:44 +0800

    0

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://buy.localweb.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://buy.localweb.com/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/git/buy/catalog/');
+define('DIR_SYSTEM', 'D:/git/buy/system/');
+define('DIR_LANGUAGE', 'D:/git/buy/catalog/language/');
+define('DIR_TEMPLATE', 'D:/git/buy/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/git/buy/system/config/');
+define('DIR_IMAGE', 'D:/git/buy/image/');
+define('DIR_CACHE', 'D:/git/buy/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/git/buy/system/storage/download/');
+define('DIR_LOGS', 'D:/git/buy/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/git/buy/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/git/buy/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'xiong');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+

f4955e4820ed3821f86cfde7f2d02c6c488f7a6b
wang <ccna8008@163.com>
2019-03-20 21:37:50 +0800

    还未提交
    2019年3月20日,等待合并

--- admin/config.php
+++ admin/config.php
@@ -26,7 +26,7 @@ define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'xiong');
+define('DB_DATABASE', 'buy_online');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 

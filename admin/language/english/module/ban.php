e2ab175aee81230e3460a217adc9c2f2ded0d633
wang <ccna8008@163.com>
2019-04-13 23:39:38 +0800

    Update ban.php

--- admin/language/english/module/ban.php
+++ admin/language/english/module/ban.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']    = 'ban';
+$_['heading_title']    = 'site browsing passwd';
 
 $_['text_module']      = 'Modules';
 $_['text_success']     = 'Success: You have modified ban module!';

a51a0747501424230d7588a49db97ae74bdaa6ec
mrg123 <ccna8008@163.com>
2019-10-04 22:03:35 +0800

    mac update

--- config.php
+++ config.php
@@ -6,23 +6,23 @@ define('HTTP_SERVER', 'http://buy.localweb.com/');
 define('HTTPS_SERVER', 'http://buy.localweb.com/');
 
 // DIR
-define('DIR_APPLICATION', 'D:/git/buy/catalog/');
-define('DIR_SYSTEM', 'D:/git/buy/system/');
-define('DIR_LANGUAGE', 'D:/git/buy/catalog/language/');
-define('DIR_TEMPLATE', 'D:/git/buy/catalog/view/theme/');
-define('DIR_CONFIG', 'D:/git/buy/system/config/');
-define('DIR_IMAGE', 'D:/git/buy/image/');
-define('DIR_CACHE', 'D:/git/buy/system/storage/cache/');
-define('DIR_DOWNLOAD', 'D:/git/buy/system/storage/download/');
-define('DIR_LOGS', 'D:/git/buy/system/storage/logs/');
-define('DIR_MODIFICATION', 'D:/git/buy/system/storage/modification/');
-define('DIR_UPLOAD', 'D:/git/buy/system/storage/upload/');
+define('DIR_APPLICATION', '/Users/wanghualong/web/github/buy/catalog/');
+define('DIR_SYSTEM', '/Users/wanghualong/web/github/buy/system/');
+define('DIR_LANGUAGE', '/Users/wanghualong/web/github/buy/catalog/language/');
+define('DIR_TEMPLATE', '/Users/wanghualong/web/github/buy/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/wanghualong/web/github/buy/system/config/');
+define('DIR_IMAGE', '/Users/wanghualong/web/github/buy/image/');
+define('DIR_CACHE', '/Users/wanghualong/web/github/buy/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/wanghualong/web/github/buy/system/storage/download/');
+define('DIR_LOGS', '/Users/wanghualong/web/github/buy/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/wanghualong/web/github/buy/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/wanghualong/web/github/buy/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
+define('DB_HOSTNAME', '127.0.0.1');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'rootroot');
 define('DB_DATABASE', 'buy_online');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');

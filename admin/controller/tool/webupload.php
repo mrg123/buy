7b527620a081eb99249e243c10f150633d74f5c0
wang <ccna8008@163.com>
2019-12-15 19:18:36 +0800

    更新图片上传功能

--- admin/controller/tool/webupload.php
+++ admin/controller/tool/webupload.php
@@ -56,108 +56,113 @@ class ControllerToolWebupload extends Controller {
 	}
 
 	public function upload(){
-		$order_id = $this->request->get['order_id'];	
-		$num = $this->request->get['num'];	
+        try {
+            $order_id = $this->request->get['order_id'];
+            $num = $this->request->get['num'];
 
-		$this->init();
+            $this->init();
 
-		$targetDir = DIR_IMAGE . 'order_upload_tmp' . DIRECTORY_SEPARATOR . $order_id;
-		$uploadDir = DIR_IMAGE . 'order_upload' . DIRECTORY_SEPARATOR . $order_id;
-		// Create target dir
-		if (!file_exists($targetDir)) {
-		@mkdir($targetDir);
+            $targetDir = DIR_IMAGE . 'order_upload_tmp' . DIRECTORY_SEPARATOR . $order_id;
+            $uploadDir = DIR_IMAGE . 'order_upload' . DIRECTORY_SEPARATOR . $order_id;
+            // Create target dir
+            if (!file_exists($targetDir)) {
+                @mkdir($targetDir);
+            }
+
+            // Create target dir
+            if (!file_exists($uploadDir)) {
+                @mkdir($uploadDir);
+            }
+            $date = date('Y-m-d_H-i-s');
+            // Get a file name
+            if (isset($_REQUEST["name"])) {
+                $fileName = uniqid($date  . "_") .'.'. pathinfo($_REQUEST["name"])['extension'];
+            } elseif (!empty($_FILES)) {
+                $fileName = $_FILES["file"]["name"];
+            } else {
+                $fileName = uniqid("file_");
+            }
+            $img_url = 'image'.DIRECTORY_SEPARATOR.'order_upload' . DIRECTORY_SEPARATOR . $order_id . DIRECTORY_SEPARATOR . $fileName;
+        
+
+            $filePath = $targetDir . DIRECTORY_SEPARATOR . $fileName;
+            $uploadPath = $uploadDir . DIRECTORY_SEPARATOR . $fileName;
+
+            // Chunking might be enabled
+            $chunk = isset($_REQUEST["chunk"]) ? intval($_REQUEST["chunk"]) : 0;
+            $chunks = isset($_REQUEST["chunks"]) ? intval($_REQUEST["chunks"]) : 1;
+
+            // Open temp file
+            if (!$out = @fopen("{$filePath}_{$chunk}.parttmp", "wb")) {
+                die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+            }
+
+            if (!empty($_FILES)) {
+                if ($_FILES["file"]["error"] || !is_uploaded_file($_FILES["file"]["tmp_name"])) {
+                    die('{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}');
+                }
+
+                // Read binary input stream and append it to temp file
+                if (!$in = @fopen($_FILES["file"]["tmp_name"], "rb")) {
+                    die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+                }
+            } else {
+                if (!$in = @fopen("php://input", "rb")) {
+                    die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+                }
+            }
+
+            while ($buff = fread($in, 4096)) {
+                fwrite($out, $buff);
+            }
+
+            @fclose($out);
+            @fclose($in);
+
+            rename("{$filePath}_{$chunk}.parttmp", "{$filePath}_{$chunk}.part");
+
+            $index = 0;
+            $done = true;
+            for ($index = 0; $index < $chunks; $index++) {
+                if (!file_exists("{$filePath}_{$index}.part")) {
+                    $done = false;
+                    break;
+                }
+            }
+            if ($done) {
+                if (!$out = @fopen($uploadPath, "wb")) {
+                    die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+                }
+
+                if (flock($out, LOCK_EX)) {
+                    for ($index = 0; $index < $chunks; $index++) {
+                        if (!$in = @fopen("{$filePath}_{$index}.part", "rb")) {
+                            break;
+                        }
+
+                        while ($buff = fread($in, 4096)) {
+                            fwrite($out, $buff);
+                        }
+
+                        @fclose($in);
+                        @unlink("{$filePath}_{$index}.part");
+                    }
+
+                    flock($out, LOCK_UN);
+                }
+                @fclose($out);
+            }
+
+            // 图片上传成功,写入到订单图片表中
+            $this->load->model('tool/order_img');
+            $this->model_tool_order_img->add($order_id, $img_url, $num);
+        
+            // Return Success JSON-RPC response
+            die('{"jsonrpc" : "2.0", "result" : null, "id" : "id"}');
+        }catch(Exception $e){
+			die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "'.$e->getMessage().'"}, "id" : "id"}');	
+
 		}
-
-		// Create target dir
-		if (!file_exists($uploadDir)) {
-		@mkdir($uploadDir);
-		}
-		$date = date('Y-m-d_H-i-s');
-		// Get a file name
-		if (isset($_REQUEST["name"])) {
-			$fileName = uniqid($date  . "_") .'.'. pathinfo($_REQUEST["name"])['extension'];
-		} elseif (!empty($_FILES)) {
-			$fileName = $_FILES["file"]["name"];
-		} else {
-			$fileName = uniqid("file_");
-		}
-		$img_url = 'image'.DIRECTORY_SEPARATOR.'order_upload' . DIRECTORY_SEPARATOR . $order_id . DIRECTORY_SEPARATOR . $fileName;
-		
-
-		$filePath = $targetDir . DIRECTORY_SEPARATOR . $fileName;
-		$uploadPath = $uploadDir . DIRECTORY_SEPARATOR . $fileName;
-
-				// Chunking might be enabled
-		$chunk = isset($_REQUEST["chunk"]) ? intval($_REQUEST["chunk"]) : 0;
-		$chunks = isset($_REQUEST["chunks"]) ? intval($_REQUEST["chunks"]) : 1;
-
-				// Open temp file
-		if (!$out = @fopen("{$filePath}_{$chunk}.parttmp", "wb")) {
-			die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
-		}
-
-		if (!empty($_FILES)) {
-			if ($_FILES["file"]["error"] || !is_uploaded_file($_FILES["file"]["tmp_name"])) {
-				die('{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}');
-			}
-
-			// Read binary input stream and append it to temp file
-			if (!$in = @fopen($_FILES["file"]["tmp_name"], "rb")) {
-				die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
-			}
-		} else {
-			if (!$in = @fopen("php://input", "rb")) {
-				die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
-			}
-		}
-
-		while ($buff = fread($in, 4096)) {
-			fwrite($out, $buff);
-		}
-
-		@fclose($out);
-		@fclose($in);
-
-		rename("{$filePath}_{$chunk}.parttmp", "{$filePath}_{$chunk}.part");
-
-		$index = 0;
-		$done = true;
-		for( $index = 0; $index < $chunks; $index++ ) {
-			if ( !file_exists("{$filePath}_{$index}.part") ) {
-				$done = false;
-				break;
-			}
-		}
-		if ( $done ) {
-			if (!$out = @fopen($uploadPath, "wb")) {
-				die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
-			}
-
-			if ( flock($out, LOCK_EX) ) {
-				for( $index = 0; $index < $chunks; $index++ ) {
-					if (!$in = @fopen("{$filePath}_{$index}.part", "rb")) {
-						break;
-					}
-
-					while ($buff = fread($in, 4096)) {
-						fwrite($out, $buff);
-					}
-
-					@fclose($in);
-					@unlink("{$filePath}_{$index}.part");
-				}
-
-				flock($out, LOCK_UN);
-			}
-			@fclose($out);
-		}
-
-		// 图片上传成功,写入到订单图片表中
-		$this->load->model('tool/order_img');
-		$this->model_tool_order_img->add($order_id,$img_url,$num);	
-
-		// Return Success JSON-RPC response
-		die('{"jsonrpc" : "2.0", "result" : null, "id" : "id"}');
 	}
 
 	public function preview(){
